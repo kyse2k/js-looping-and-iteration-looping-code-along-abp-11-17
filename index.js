4a8c8655620e39d8118bc19ad8a4ca41c49149f8
Sérgio Pereira <sergiodanielpereira@gmail.com>
2017-11-12 22:24:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 // Code your solutions in this file
 
-
-let arr = ['bob', 'jill', 'rob'];
 function printBadges(arr) {
   for (let i = 0; i < arr.length; i++) {
     console.log(`Welcome ${arr[i]}! You are employee #${i+1}`)
