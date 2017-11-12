d8db93c4bf4bc4d42155f0eb655e7a113d91a4bf
Sérgio Pereira <sergiodanielpereira@gmail.com>
2017-11-12 22:22:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solutions in this file
+
+
+let arr = ['bob', 'jill', 'rob'];
+function printBadges(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(`Welcome ${arr[i]}! You are employee #${i+1}`)
+  }
+  return arr
+}
